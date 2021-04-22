427addcb3a5f48b2dab3cf527591c5892ab7e92e
Maxwell <maxwell.knoxx@live.com>
2021-04-22 19:36:45 -0300

    Configuring hr-config-server project

--- hr-config-server/src/main/java/com/maxwell/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/maxwell/hrconfigserver/HrConfigServerApplication.java
@@ -2,7 +2,10 @@ package com.maxwell.hrconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+
+@EnableConfigServer
 @SpringBootApplication
 public class HrConfigServerApplication {
 
