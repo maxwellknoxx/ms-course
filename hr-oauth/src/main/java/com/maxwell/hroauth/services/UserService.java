08326d4abc0f912abbd74fd6fb776d31009ad261
Maxwell <maxwell.knoxx@live.com>
2021-04-26 22:07:28 -0300

    login, JWT Token

--- hr-oauth/src/main/java/com/maxwell/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/maxwell/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.maxwell.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.maxwell.hroauth.entities.User;
 import com.maxwell.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
@@ -23,6 +26,17 @@ public class UserService {
 			throw new IllegalArgumentException("E-mail not found");
 		}
 		logger.info("E-mail found: " + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if (user == null) {
+			logger.error("Username not found: " + username);
+			throw new UsernameNotFoundException("Username not found");
+		}
+		logger.info("E-mail found: " + username);
 		return user;
 	}
 
